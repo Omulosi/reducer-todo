434b6a8230bec2c2b9843dd740503fb655ab611e
Omulosi <mulongojohnpaul@gmail.com>
2019-10-08 20:27:24 +0300

    Update TodoItem component

--- reducer-todo/src/components/TodoItem.js
+++ reducer-todo/src/components/TodoItem.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 
 const TodoItem = (props) => {
+  const { markComplete, clearCompleted} = props;
+  const strike = props.item.complete? 'mark-complete': '';
   return (
     <div className="todo-item">
-      <p>{props.item.item}</p>
-      <button onClick={props.markComplete}>Mark Complete</button>
+      <p className={strike}>{props.item.name}</p>
+      <button onClick={(e) => markComplete(props.item.id, true)}>Mark Complete</button>
+      <button onClick={(e) => clearCompleted(props.item.id)}>Clear</button>
     </div>
   )
 }
